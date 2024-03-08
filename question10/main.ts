fd096e063bad31cb76360d669604cd52d2809e61
DevXAhad <abdulahad69471@gmail.com>
2024-03-09 03:41:24 +0500

    font changes updated in settings

--- question10/main.ts
+++ question10/main.ts
@@ -1,5 +1,5 @@
 // Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
-/* let personName: string = "\t Abdul Ahad \n";
+let personName: string = "\t Abdul Ahad \n";
 console.log("Name with whitespaces:", personName)
 let strippedName: string = personName.trim() //trim function removes whitespaces
 console.log("Name without whitespaces:",strippedName);
@@ -10,4 +10,4 @@ console.log("-".repeat(18))
 console.log(8 / 1)
 console.log("-".repeat(18))
 console.log(16 - 8)
-console.log("-".repeat(18)) */
+console.log("-".repeat(18)) 
